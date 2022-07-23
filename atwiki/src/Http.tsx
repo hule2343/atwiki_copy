a3135a5fc37d1224a30300ac55f83a62d5df4823
hule2343 <aaa402262@gmail.com>
2022-07-23 20:11:30 +0900

    フロントの名前・日付・更新ログのあるものを追加

--- atwiki/src/Http.tsx
+++ atwiki/src/Http.tsx
@@ -4,19 +4,27 @@ import axios, { AxiosResponse } from "axios";
 // モックサーバーのURL　db.json
 const membersUrl = "http://localhost:3100/members";
 const workdayUrl = "http://localhost:3100/workday";
+const restUrl = "http://localhost:3100/rest";
+const logUrl = "http://localhost:3100/log";
 
 type Member = {
-  id: string;
+  id: number;
   name: string;
   address: string;
   task: string;
 };
 
 type WorkDay = {
-  id: string;
+  id: number;
   day: string;
   place: string;
   rest: Member[];
+};
+
+type Log = {
+  id: number;
+  url: string;
+  date: string;
 };
 
 export const MemberList: React.FC = () => {
@@ -50,12 +58,31 @@ export const MemberList: React.FC = () => {
   );
 };
 
+const applyRest = () => {
+  axios.post("http://localhost:3100/rest", { id: 2, name: "ttaira" });
+  window.location.reload();
+  LeaveLog("http://localhost:3100/rest");
+};
+
+const attendWork = () => {
+  axios.delete("http://localhost:3100/rest/2");
+  window.location.reload();
+  LeaveLog("http://localhost:3100/rest");
+};
+
 export const WorkDayList: React.FC = () => {
   const [workdays, setWork] = React.useState<WorkDay[]>([]);
+  const [rest, setRest] = React.useState<Member[]>([]);
 
   React.useEffect(() => {
     axios.get(workdayUrl).then((response) => {
       setWork(response.data);
+    });
+  }, []);
+
+  React.useEffect(() => {
+    axios.get(restUrl).then((response) => {
+      setRest(response.data);
     });
   }, []);
 
@@ -74,9 +101,11 @@ export const WorkDayList: React.FC = () => {
             <td>{workday.day}</td>
             <td>{workday.place}</td>
             <td>
-              {workday.rest.map((worker) => (
-                <br>{worker.name}</br>
+              {rest.map((worker) => (
+                <li key={worker.id}>{worker.name}</li>
               ))}
+              <button onClick={applyRest}>休む</button>
+              <button onClick={attendWork}>出席する</button>
             </td>
           </tr>
         ))}
@@ -85,6 +114,32 @@ export const WorkDayList: React.FC = () => {
   );
 };
 
-export const Log: React.FC = () => {
-  return <li></li>;
+const LeaveLog = (url: string) => {
+  let now = new Date();
+  let date = now.toString();
+  axios.post("http://localhost:3100/log", { url: url, date: date });
+};
+
+export const Loglist: React.FC = () => {
+  const [logs, setLog] = React.useState<Log[]>([]);
+
+  React.useEffect(() => {
+    axios.get(logUrl).then((response) => {
+      setLog(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h3>更新履歴</h3>
+      <h4>
+        {logs.map((log) => (
+          <div>
+            <li key={log.id}>{log.date}</li>
+            <a href={log.url}>編集箇所</a>
+          </div>
+        ))}
+      </h4>
+    </div>
+  );
 };
--- atwiki/src/index.tsx
+++ atwiki/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import { MemberList, WorkDayList, Log } from "./Http";
+import { MemberList, WorkDayList, Loglist } from "./Http";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
@@ -15,7 +15,7 @@ root.render(
         <WorkDayList />
       </div>
       <div>
-        <Log />
+        <Loglist />
       </div>
     </div>
   </React.StrictMode>
