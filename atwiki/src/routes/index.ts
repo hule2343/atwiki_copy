25e334db8f0051f10de030d4083b91c2160b9051
hule2343 <aaa402262@gmail.com>
2022-09-25 11:27:58 +0900

    #1 import文に拡張子を追記

--- atwiki/src/routes/index.ts
+++ atwiki/src/routes/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
-import userRouter from "./crud/user";
-import logRouter from "./crud/log";
-import authRouter from "./auth/auth";
+import userRouter from "./crud/user.js";
+import logRouter from "./crud/log.js";
+import authRouter from "./auth/auth.js";
 import { PrismaClient } from "@prisma/client";
 import session from "express-session";
 import cors from "cors";
