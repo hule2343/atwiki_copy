4038094ff16bd68e6b9875fc34f2128b38a6edec
hule2343 <aaa402262@gmail.com>
2022-11-05 18:27:58 +0900

    #26 add validation and 409 error response to sign up form

--- atwiki/src/sign_up_Form.tsx
+++ atwiki/src/sign_up_Form.tsx
@@ -3,12 +3,16 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { axios, discordUrl } from "./Http";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { response } from "express";
 
 type UserForm = {
   name: string;
   email: string;
   phonenumber: string;
   password: string;
+  password_repeat: string;
   is_student: boolean;
 };
 
@@ -25,10 +29,25 @@ export const CreateUserForm = () => {
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
-  } = useForm<UserForm>();
+  } = useForm<UserForm>({ mode: "onChange" });
 
   const [is_student, setBoolean] = useState(false);
+
+  const [shown, setPasswordShown] = useState(false);
+
+  const eye = <FontAwesomeIcon icon={faEye} />;
+
+  const password = React.useRef({});
+
+  const [resError, setError] = React.useState<string>("");
+
+  password.current = watch("password", "");
+
+  const togglePasswordVisiblity = () => {
+    setPasswordShown(!shown);
+  };
 
   const onSubmit = (data: UserForm): void => {
     axios
@@ -43,8 +62,11 @@ export const CreateUserForm = () => {
         leaveSingupLog(data.name);
       })
       .catch((error) => {
+        if (error.response.status === 409) {
+          setError("既に登録されているアカウントと重複があります");
+        }
         if (error.response) {
-          console.log(error);
+          console.log(error.response);
         }
       });
   };
@@ -59,6 +81,7 @@ export const CreateUserForm = () => {
     const logData: Log = await axios.get("/logs");
     const url = `http://localhost:3001/logs/${logData.id + 1}`;
     const title = `${name}さんがatwikiに登録されました`;
+
     await axios.post("/logs/log", {
       date: date,
       url: url,
@@ -72,28 +95,39 @@ export const CreateUserForm = () => {
 
   return (
     <div>
+      <div>
+        <span style={{ color: "red" }}>*</span>は必須項目です
+      </div>
       <form onSubmit={handleSubmit(onSubmit)} className="ms-3">
         <div className="mt-2">
           <label htmlFor="name" className="form-label">
-            名前
+            <span style={{ color: "red" }}>*</span>名前
           </label>
           <input
             id="name"
             className="form-control w-auto"
-            {...register("name", { required: true })}
+            placeholder="関技　太郎"
+            {...register("name", { required: "必須事項です" })}
           />
-          {errors.name && <div>必須項目です</div>}
+          {errors.name && errors.name.message}
         </div>
         <div className="mt-2">
           <label htmlFor="email" className="form-label">
-            メールアドレス
+            <span style={{ color: "red" }}>*</span>メールアドレス
           </label>
           <input
             id="email"
             className="form-control w-auto"
-            {...register("email", { required: true })}
+            placeholder="kangi@email.com"
+            {...register("email", {
+              required: "必須項目です",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: "有効なアドレスを入力してください",
+              },
+            })}
           />
-          {errors.name && <div>必須項目です</div>}
+          {errors.email && errors.email.message}
         </div>
         <div className="mt-2">
           <label htmlFor="phonenumber" className="form-label">
@@ -102,19 +136,44 @@ export const CreateUserForm = () => {
           <input
             id="phonenumber"
             className="form-control w-auto"
+            placeholder="000-0000-0000"
             {...register("phonenumber")}
           />
         </div>
         <div className="mt-2">
           <label htmlFor="password" className="form-label">
-            パスワード
+            <span style={{ color: "red" }}>*</span>パスワード
           </label>
           <input
             id="password"
             className="form-control w-auto"
-            {...register("password", { required: true })}
+            type={shown ? "text" : "password"}
+            placeholder={"６文字以上"}
+            {...register("password", {
+              required: "必須項目です",
+              minLength: { value: 6, message: "6文字以上入力してください" },
+            })}
+          />
+
+          {errors.password && errors.password.message}
+        </div>
+        <div className="mt-2">
+          <label htmlFor="password_repeat" className="form-label">
+            <span style={{ color: "red" }}>*</span>パスワード確認用
+          </label>
+          <input
+            id="password_repeat"
+            className="form-control w-auto"
+            type={shown ? "text" : "password"}
+            {...register("password_repeat", {
+              required: "必須項目です",
+              validate: (value) =>
+                value === password.current || "パスワードが一致しません",
+            })}
           />
         </div>
+        {errors.password_repeat && errors.password_repeat.message}
+        <i onClick={togglePasswordVisiblity}>{eye}</i>
         <div className="mt-2">
           <input
             type="checkbox"
@@ -123,10 +182,12 @@ export const CreateUserForm = () => {
           />
           <label className="form-check-label ms-2">学生</label>
         </div>
+
         <button type="submit" className="btn btn-outline-primary mt-2">
           Sign up
         </button>
       </form>
+      <div>{resError}</div>
     </div>
   );
 };
