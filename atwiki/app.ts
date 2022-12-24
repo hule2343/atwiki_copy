13468432d795fb64942cff920619d2eb080070d2
hule2343 <aaa402262@gmail.com>
2022-12-24 17:39:07 +0900

    #38 success to start app and access login form in container

--- atwiki/app.ts
+++ atwiki/app.ts
@@ -10,13 +10,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-    optionsSuccessStatus: 200,
-  })
-);
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }), express.json());
 
@@ -41,8 +35,9 @@ app.use("/users", userRouter);
 app.use("/logs", logRouter);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(3001);
